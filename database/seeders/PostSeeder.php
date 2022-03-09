796827e2c98da9110f9ee27faf34c99c2aad7bbc
ariqluth <ttzluthfi@gmail.com>
2022-03-09 22:20:42 +0700

    pertemuan 4 percobaan seeding

---
+++ database/seeders/PostSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Support\Facades\DB;
+use Illuminate\Database\Seeder;
+
+class PostSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run() 
+    {
+        DB::table('posts')->insert([
+            'id' => 1,
+            'title' => 'Belajar Model Dengan Laravel',
+            'isian' => 'lLorem ipsum dolor sit, amet consectetur adipisicing elit. Omnis consequatur iste laborum cupiditate molestiae amet et at sunt beatae impedit?',
+            'gambar' => 'https://via.placeholder.com/640x480.png/00ff77?text=Belajar Model'
+            
+        ]);
+    }
+}
