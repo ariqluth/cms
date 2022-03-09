cf0746979b356101453ab22a9ae65593beaf3cd8
ariqluth <ttzluthfi@gmail.com>
2022-03-09 21:31:27 +0700

    pertemuan 4 memanggil gambar pada databases

--- database/migrations/2022_03_09_084620_create_posts_table.php
+++ database/migrations/2022_03_09_084620_create_posts_table.php
@@ -17,6 +17,7 @@ class CreatePostsTable extends Migration
             $table->id();
             $table->string('title', 100)->index();
             $table->text('isian');
+            $table->string('gambar');
         });
     }
 
--- resources/views/slider.blade.php
+++ resources/views/slider.blade.php
@@ -19,28 +19,28 @@
       <div class="slider">
       		    <ul class="slides">
           	 	 <li class="slide">
+                         {{-- perulangan buat manggil databases --}}
+                         @foreach($pasta as $pa)
                     <div class="item">
-                          <img src= "{{ asset ('images2/pasta[gambar]')}}" width="226" height="225" alt="sliderimg" class="wow flipInX"
-                           data-wow-delay=".8s">
-                          
-                           {{-- perulangan buat manggil databases --}}
-                           @foreach($pasta as $pa) 
+                         
+                        <img src="{{ asset ('images2/'.$pa->gambar)}}" width="226" height="225" alt="sliderimg" class="wow flipInX"
+                        data-wow-delay=".8s"> 
                           <h3>{{$pa->title}}</h3>
-                          @endforeach
-
+                        
                       </div> <!-- end of item-->
-                      
+                  
                    <div class="item2">
-                          <img src="{{ asset ('images2/thumb2.jpg')}}" width="226" height="225" alt="sliderimg" class="wow flipInX"
+                          <img src="{{ asset ('images2/'.$pa->gambar)}}" width="226" height="225" alt="sliderimg" class="wow flipInX"
                            data-wow-delay=".8s"> 
-                          <h3>Pasta Pillus</h3>
+                           <h3>{{$pa->title}}</h3>
                       </div> <!-- end of item-->
                       
                    <div class="item3">
-                          <img src="{{ asset ('images2/thumb3.png')}}" width="226" height="225" alt="sliderimg" class="wow flipInX"
+                          <img src="{{ asset ('images2/'.$pa->gambar)}}" width="226" height="225" alt="sliderimg" class="wow flipInX"
                            data-wow-delay=".8s"> 
-                          <h3>Pasta Fusilli</h3>
+                           <h3>{{$pa->title}}</h3>
                       </div> <!-- end of item-->
+                      @endforeach
                   </li>
                    <li class="slide">
                     <div class="item">
